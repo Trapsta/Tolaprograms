7ddffd6026388c9ee5ee1596b9f7a9576e4bd105
unknown <jasper.kinoti@gmail.com>
2018-11-04 04:00:20 +0300

    HTTP Config

---
+++ src/app/utils/config.ts
@@ -0,0 +1,6 @@
+import { Headers  } from '@angular/http';
+
+export const config = new Headers({
+        'Authorization': 'Bearer GfR6vIHG0zTWaJle6TjNXvYUrjDn6g',
+        'Content-Type': 'application/json'
+});
